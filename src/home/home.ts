5f7510ec3a9b11e85b3e18a2ff1740f2a901c084
Jeremy Zacharia <jczacharia@gmail.com>
2019-06-21 19:21:08 -0400

    append

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,4 +1,10 @@
-import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -7,7 +13,6 @@ import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angu
   encapsulation: ViewEncapsulation.None
 })
 export class Home {
-
   isMobile: boolean;
   constructor() {
     if (window.screen.width < 768) {
@@ -17,30 +22,29 @@ export class Home {
     }
   }
 
+  year = new Date().getFullYear();
+
   title = 'app';
   tileTitle: string;
   appTileArray: AppTile[] = [
-    { title: 'Resume', subtitle: ".PDF..." },
-    { title: 'Bio', subtitle: "My Story..." },
-    { title: 'Skills', subtitle: "Projects, etc..." },
-    { title: 'Experience', subtitle: "Jobs..." },
-    { title: 'Contact', subtitle: "Email, etc..." },
+    { title: 'Resume', subtitle: '.PDF...' },
+    { title: 'Bio', subtitle: 'My Story...' },
+    { title: 'Skills', subtitle: 'Projects, etc...' },
+    { title: 'Experience', subtitle: 'Jobs...' },
+    { title: 'Contact', subtitle: 'Email, etc...' }
   ];
 
   @Output() tileType = new EventEmitter<string>();
 
   expandTile(p) {
-
     this.tileType.emit(p);
     if (this.tileTitle === p) {
-      this.tileTitle = "";
+      this.tileTitle = '';
     } else {
       this.tileType.emit(p);
       this.tileTitle = p;
     }
   }
-
-
 }
 
 export class AppTile {
@@ -48,36 +52,38 @@ export class AppTile {
   subtitle: string;
 }
 
-
-
-
-
 @Component({
   selector: 'Resume',
   templateUrl: './Tiles/Resume.html'
 })
-export class Resume { }
+export class Resume {}
 
 @Component({
   selector: 'Bio',
   templateUrl: './Tiles/Bio.html'
 })
-export class Bio { age = Math.floor(((Math.abs(Date.now() - new Date("1995-10-06T12:00:00.753Z").getTime())) / (1000 * 3600 * 24)) / 365); }
+export class Bio {
+  age = Math.floor(
+    Math.abs(Date.now() - new Date('1995-10-06T12:00:00.753Z').getTime()) /
+      (1000 * 3600 * 24) /
+      365
+  );
+}
 
 @Component({
   selector: 'Skills',
   templateUrl: './Tiles/Skills.html'
 })
-export class Skills { }
+export class Skills {}
 
 @Component({
   selector: 'Experience',
   templateUrl: './Tiles/Experience.html'
 })
-export class Experience { }
+export class Experience {}
 
 @Component({
   selector: 'Contact',
   templateUrl: './Tiles/Contact.html'
 })
-export class Contact { }
+export class Contact {}
